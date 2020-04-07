c524e4b8c5d175bcb116d8193f588ad34130cc78
Mr丶Cps <peishen.cai@foxmail.com>
2020-04-07 15:18:19 +0800

    初始化框架 http://127.0.0.1:8001/

---
+++ db/cps_generator.sql
@@ -0,0 +1,66 @@
+/*
+SQLyog Ultimate v8.32 
+MySQL - 5.5.19 : Database - cps_generator
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`cps_generator` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `cps_generator`;
+
+/*Table structure for table `cps_role` */
+
+DROP TABLE IF EXISTS `cps_role`;
+
+CREATE TABLE `cps_role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '角色ID',
+  `role_name` varchar(200) DEFAULT NULL COMMENT '角色名',
+  `gmt_create` datetime DEFAULT NULL COMMENT '创建时间',
+  `gmt_modified` datetime DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `cps_role` */
+
+/*Table structure for table `cps_user` */
+
+DROP TABLE IF EXISTS `cps_user`;
+
+CREATE TABLE `cps_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户ID',
+  `user_name` varchar(200) DEFAULT NULL COMMENT '用户名',
+  `password` varchar(200) DEFAULT NULL COMMENT '用户密码',
+  `gmt_create` datetime DEFAULT NULL COMMENT '创建时间',
+  `gmt_modified` datetime DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `cps_user` */
+
+/*Table structure for table `cps_user_role` */
+
+DROP TABLE IF EXISTS `cps_user_role`;
+
+CREATE TABLE `cps_user_role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户角色ID',
+  `user_id` int(11) DEFAULT NULL COMMENT '用户ID',
+  `role_id` int(11) DEFAULT NULL COMMENT '角色ID',
+  `gmt_create` datetime DEFAULT NULL COMMENT '创建时间',
+  `gmt_modified` datetime DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `cps_user_role` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
